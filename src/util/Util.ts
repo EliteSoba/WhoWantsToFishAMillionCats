44c0bc4442f5947ee428bd8c72359af7f44dcc02
EliteSoba <tobiasblee@gmail.com>
2026-02-16 14:49:18 -0800

    Add untracked items

---
+++ src/util/Util.ts
@@ -0,0 +1,26 @@
+// Source - https://stackoverflow.com/a/12646864
+// Posted by Laurens Holst, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-16, License - CC BY-SA 4.0
+export function shuffleArray<Type>(array: Type[]) {
+  if (!array.length) {
+    return array;
+  }
+  const clonedArray = [...array];
+  for (let i = clonedArray.length - 1; i > 0; i--) {
+    const j = randomInt(i + 1);
+    [clonedArray[i], clonedArray[j]] = [clonedArray[j], clonedArray[i]];
+  }
+  return clonedArray;
+}
+
+/**
+ * Returns a random number from 0 inclusive to range exclusive, so [0, range)
+ */
+export function randomInt(range: number) {
+  return Math.floor(Math.random() * range);
+}
+
+export function resetLocation() {
+  // TODO: if i do react router or something this'll need to change but will probably be easier
+  window.history.replaceState({}, '', document.location.href.replace(document.location.search, ''));
+}
